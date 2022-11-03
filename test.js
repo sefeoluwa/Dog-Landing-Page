89eb7845e7dff8c28e956116d2bfba3b5f3da0bf
sefeoluwa <sefeoluwa01@gmail.com>
2022-11-03 21:36:30 +0100

    JavaScript test file works

--- test.js
+++ test.js
@@ -3,5 +3,5 @@ var action = document.querySelector('#action');
 var newDiv = document.createElement('div');
 newDiv.setAttribute('id', 'newcarousel');
 newDiv.classList.toggle('newCarousel');
-newDiv.textContent = 'Hello everybody!';
+newDiv.textContent = 'You rock!';
 action.appendChild(newDiv)
